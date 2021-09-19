da029438268ec1b9461fae4d7721149e12c8f186
comghana <ji970322@naver.com>
2021-09-19 21:42:50 +0900

    backup coin.java

--- 02_java/src/coin.java
+++ 02_java/src/coin.java
@@ -1,4 +1,6 @@
 // 거스름 돈: 최소한의 동전 개수
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class coin {
@@ -6,15 +8,24 @@ public class coin {
   public static void main(String[] args) {
 
     Scanner scan = new Scanner(System.in);
-    System.out.print("거스름돈의 금액을 입력하세요: ");
-    int money = scan.nextInt();
-//    int n = 1260;
-    int cnt = 0;
-    int[] coin_type = {500, 100, 50, 10};
+    System.out.print("동전 종류의 개수를 입력해주세요: ");
+    int coin_cnt = scan.nextInt();
 
-    for (int j : coin_type) {
-      cnt += money / j;
-      money %= j;
+    System.out.print("금액을 입력해주세요: ");
+    int n = scan.nextInt();
+
+    Integer [] type_array = new Integer [coin_cnt];
+
+    for (int i = 0; i < coin_cnt; i++){
+      System.out.print("동전의 종류를 입력해주세요: ");
+      type_array[i] = scan.nextInt();
+    }
+    Arrays.sort(type_array, Collections.reverseOrder());
+
+    int cnt = 0;
+    for (int k : type_array) {
+      cnt += n / k;
+      n %= k;
     }
     System.out.println("최소 동전 개수: "+cnt);
   }
--- 03_java/src/ATM.java
+++ 03_java/src/ATM.java
@@ -7,12 +7,19 @@ public class ATM {
     Scanner scann = new Scanner(System.in);
     int n = scann.nextInt();
 
+    int[] p = new int[n];
+    p = Array.sort(p);
+
+    for (int i = 0; i < n; i++){
+      p[i] = scann.nextInt();
+    }
+
     int result = 0;
     int temp = 0;
 
     for (int i = 0; i <n; i++){
-      int p = scann.nextInt();
-      temp += p;
+
+      temp += p[i];
       result += temp;
     }
     System.out.println(result);
